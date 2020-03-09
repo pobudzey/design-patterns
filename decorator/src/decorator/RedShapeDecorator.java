4b1a44bc9db6cc6aba8bea98403801a61210dcb9
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2020-03-09 17:19:17 -0400

    Added case study for the Decorator pattern

---
+++ decorator/src/decorator/RedShapeDecorator.java
@@ -0,0 +1,20 @@
+package decorator;
+
+public class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    public void setRedBorder(Shape decoratedShape) {
+        System.out.println("Border color: Red");
+    }
+
+
+}
